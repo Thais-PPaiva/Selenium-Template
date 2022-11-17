80a1109331d040f67f7f19eaf33150c8e51865d8
Thaís Paiva <83622511+Thais-PPaiva@users.noreply.github.com>
2022-11-16 22:21:22 -0300

    Add files via upload

--- src/test/java/br/com/Template/JnuitTemplate.java
+++ src/test/java/br/com/Template/JnuitTemplate.java
@@ -13,4 +13,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 class JnuitTemplate {
 
+	private String INDEX_URL = "";
+	private WebDriver browser;
+    
+   @BeforeEach
+   public void beforeEach() {
+	  System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+	  this.browser = new ChromeDriver();
+   }
+    
+   @AfterEach
+	public void afterEach() {
+	   this.browser.close();
+   }
+   
+  @Test
+  public void Test01() throws Exception{
+	this.browser.manage().window().maximize();
+	this.browser.get(INDEX_URL);
+    WebElement botaoPaginaPrincipal = this.browser.findElement(By.id());
+    botaoPaginaPrincipal.click();
+    Thread.sleep(3500);
+    Assertions.assertEquals(INDEX_URL, this.browser.getCurrentUrl());
+  }
+  
+  @Test
+  public void Test02() throws Exception{
+	this.browser.manage().window().maximize();
+	this.browser.get(INDEX_URL);
+    WebElement botaoPaginaPrincipal = this.browser.findElement(By.id());
+    botaoPaginaPrincipal.click();
+    Thread.sleep(3500);
+    Assertions.assertEquals(, this.browser.getCurrentUrl());
+  }
+  
+  @Test
+  public void Test03() throws Exception{
+	this.browser.manage().window().maximize();
+	this.browser.get(INDEX_URL);
+    WebElement botaoPaginaPrincipal = this.browser.findElement(By.id());
+    botaoPaginaPrincipal.click();
+    Thread.sleep(3500);
+    Assertions.assertEquals(, this.browser.getCurrentUrl());
+  }
+  
+  @Test
+  public void Test04() throws Exception{
+	this.browser.manage().window().maximize();
+	this.browser.get(INDEX_URL);
+    WebElement botaoPaginaPrincipal = this.browser.findElement(By.id());
+    botaoPaginaPrincipal.click();
+    Thread.sleep(3500);
+    Assertions.assertEquals(, this.browser.getCurrentUrl());
+  }
 }
