2f3070cbcb26f2e9f72db1b6d8890483a7173ec0
Thaís Paiva <83622511+Thais-PPaiva@users.noreply.github.com>
2022-11-16 21:04:13 -0300

    Add files via upload

---
+++ src/test/java/br/com/Template/JnuitTemplate.java
@@ -0,0 +1,14 @@
+package br.com.Template;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JnuitTemplate {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
