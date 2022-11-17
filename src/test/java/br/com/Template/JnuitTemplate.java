72638a04dfd676440304fbfba3922d5cfd05acb0
Thaís Paiva <83622511+Thais-PPaiva@users.noreply.github.com>
2022-11-16 21:20:16 -0300

    Add files via upload

--- src/test/java/br/com/Template/JnuitTemplate.java
+++ src/test/java/br/com/Template/JnuitTemplate.java
@@ -2,13 +2,15 @@ package br.com.Template;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 class JnuitTemplate {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
